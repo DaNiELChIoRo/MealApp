ff084aca284ad61cc207e2d278cb8fd7fcd855a0
Daniel Meneses León <da.leon1917@gmail.com>
2023-08-22 09:48:16 -0600

    Initial Commit

---
+++ MealApp/MealAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealAppApp.swift
+//  MealApp
+//
+//  Created by Daniel Meneses León on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MealAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
